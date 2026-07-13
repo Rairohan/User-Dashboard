51e2e292b27e0562e2381836acb457ad4877ce31
rohan <rairohan14303@gmail.com>
2026-07-13 21:05:19 +0545

    Tailwind added to ExpenseList.jsx

--- src/component/ExpenseItem.jsx
+++ src/component/ExpenseItem.jsx
@@ -6,11 +6,13 @@ const ExpenseItem = React.memo(function ExpenseItem({expense})
 {
     const {dispatch} = useContext(ExpenseContext)
     return(
-        <div>
+        <div className="bg-gray-200 m-4 flex justify-center p-4 rounded-xl gap-4 text-2xl">
             <p>{expense.title}</p>
             <p>{expense.amount}</p>
             <p>{expense.category}</p>
-            <button onClick={()=>dispatch({type:"remove",payload:expense.id})}>Delete</button>
+            <button 
+            className="bg-red-500 p-1 rounded-xl text-white"
+            onClick={()=>dispatch({type:"remove",payload:expense.id})}>Delete</button>
         </div>
     )
 })
