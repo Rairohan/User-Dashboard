c9cfaaf93f5627f33d560dbbf84b003997ebbad2
rohan <rairohan14303@gmail.com>
2026-07-20 16:54:31 +0545

    Theme Css fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { ExpenseProvider } from './Context/ExpenseContent'
+import { useContext } from 'react'
+import { ExpenseProvider, ExpenseContext } from './Context/ExpenseContent'
 import AddExpense from './component/AddExpense'
 import ExpenseList from './component/ExpenseList'
 import Navbar from './component/Navbar'
 import Summary from './component/Summary'
 
-export default function App() {
+function AppContent() {
+  const { state } = useContext(ExpenseContext)
   return (
-    <ExpenseProvider>
+    <div className={`min-h-screen ${state.theme === "Dark" ? "bg-gray-900" : "bg-gray-100 text-black"}`}>
       <Navbar/>
       <AddExpense/>
       <ExpenseList/>
       <Summary/>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <ExpenseProvider>
+      <AppContent/>
     </ExpenseProvider>
   )
 }
